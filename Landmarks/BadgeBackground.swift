c97404beb70254f4e8f771df381528a4259e7424
joren <jalexander05@student.ciputra.ac.id>
2024-04-18 13:14:41 +0700

    This is a completed version of 1.1 I need to do 1.2 of the swift tutorial

---
+++ Landmarks/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
---
+++ Landmarks/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by student on 18/04/24.
+//
+
+import Foundation
