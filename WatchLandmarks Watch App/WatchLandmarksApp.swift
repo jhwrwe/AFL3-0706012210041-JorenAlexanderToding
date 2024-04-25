6059024871097bfe3612e44ebd1be3ca1f680ac8
joren <jalexander05@student.ciputra.ac.id>
2024-04-25 12:44:41 +0700

    section 4.1 finnsih just need section 4.2

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by student on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
